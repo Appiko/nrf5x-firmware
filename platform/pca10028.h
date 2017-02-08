b28e6eadaabdfe7300f4ce27e3b1a7dc35561668
EarthLord <prithvirajnarendra@gmail.com>
2017-02-08 19:22:03 +0530

    Added PCA10028 nRF51-DK to platforms

--- platform/pca10028.h
+++ platform/pca10028.h
@@ -27,28 +27,62 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PLATFORM_BOARDS_H_
-#define PLATFORM_BOARDS_H_
-
 /**
  * @addtogroup group_platform
  * @{
  *
- * @brief A @ref boards.h file includes the relevant platform based on
- *  macro from the compile time defines
+ * @defgroup board_pca10028 PCA10028 nRF51-DK
+ * @brief The nRF51 Development Kit (DK) platform PCA10028
+ * @{
  */
 
-#if defined(BOARD_PCA10040)
-  #include "pca10040.h"
-#elif defined(BOARD_PCA10028)
-  #include "pca10028.h"
-#elif defined(BOARD_CUSTOM)
-  #include "custom_board.h"
-#else
-#error "Board is not defined"
+#ifndef PLATFORM_PCA10028_H_
+#define PLATFORM_PCA10028_H_
 
-#endif
+/** @anchor pca10028-leds
+ * @name LED definitions for PCA10028
+ * @{*/
+#define LED_1          21
+#define LED_2          22
+#define LED_3          23
+#define LED_4          24
+/** The output level at which the LEDs shine */
+#define LEDS_ACTIVE_STATE 0
+/** @} */
 
-#endif /* PLATFORM_BOARDS_H_ */
+/** @anchor pca10028-buttons
+ * @name Button definitions for PCA10028
+ * @{*/
+#define BUTTON_1       17
+#define BUTTON_2       18
+#define BUTTON_3       19
+#define BUTTON_4       20
+/** The input level when the button is pressed */
+#define BUTTONS_ACTIVE_STATE 0
+/** The kind of internal resistors required for the button */
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+/** @} */
 
+/** @anchor pca10028-serial
+ * @name Serial port definitions for PCA10028
+ * @{*/
+#define RX_PIN_NUMBER  11
+#define TX_PIN_NUMBER  9
+#define CTS_PIN_NUMBER 10
+#define RTS_PIN_NUMBER 8
+/** Bool to say if the hardware flow control is required */
+#define HWFC           false
+/** @} */
+
+/** Low frequency clock source used when initializing the SoftDevice */
+#define BOARD_LFCLKSRC_STRUCT  {.source        = NRF_CLOCK_LF_SRC_XTAL,            \
+                                    .rc_ctiv       = 0,                                \
+                                    .rc_temp_ctiv  = 0,                                \
+                                    .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM}
+
+#define BOARD_LFCLKSRC         NRF_CLOCK_LFCLK_Xtal
+
+#endif /* PLATFORM_PCA10028_H_ */
+
+/** @} */
 /** @} */
