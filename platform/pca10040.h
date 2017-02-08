4745bc38de7c3e3e1b53aa26f941fcb66620c307
EarthLord <prithvirajnarendra@gmail.com>
2017-02-08 17:25:10 +0530

    Added a direct LF clk source define apart from the struct one present for SD initialization

--- platform/pca10040.h
+++ platform/pca10040.h
@@ -75,10 +75,12 @@
 /** @} */
 
 /** Low frequency clock source used when initializing the SoftDevice */
-#define NRF_CLOCK_LFCLKSRC      {.source        = NRF_CLOCK_LF_SRC_XTAL,            \
-                                 .rc_ctiv       = 0,                                \
-                                 .rc_temp_ctiv  = 0,                                \
-                                 .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM}
+#define BOARD_LFCLKSRC_STRUCT  {.source        = NRF_CLOCK_LF_SRC_XTAL,            \
+                                    .rc_ctiv       = 0,                                \
+                                    .rc_temp_ctiv  = 0,                                \
+                                    .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM}
+
+#define BOARD_LFCLKSRC         NRF_CLOCK_LFCLK_Xtal
 
 #endif /* PLATFORM_PCA10040_H_ */
 
