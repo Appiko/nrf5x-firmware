bf3e037a1c3e9cf0e0228c92da37f68d2dabeeaf
EarthLord <prithvirajnarendra@gmail.com>
2017-05-25 02:00:54 +0530

    Initial commit of the non-blocking TWI Master HAL driver

---
+++ codebase/hal/hal_twim.c
@@ -0,0 +1,296 @@
+/*
+ *  hal_twim.c
+ *
+ *  Created on: 19-May-2017
+ *
+ *  Copyright (c) 2017, Appiko
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification,
+ *  are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its contributors
+ *  may be used to endorse or promote products derived from this software without
+ *  specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ *  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "hal_twim.h"
+#include "stdbool.h"
+#include "common_util.h"
+
+static struct {
+    uint32_t scl;
+    uint32_t sda;
+    twim_transfer_t current_transfer;
+    void (*handler)(twim_err_t err, twim_transfer_t transfer);
+    bool transfer_finished;
+    bool on;
+    uint8_t evt_mask;
+}twim_status;
+
+/** @anchor rtc_defines
+ * @name Defines for the specific RTC peripheral used for ms timer
+ * @{*/
+#define TWIM_ID               CONCAT_2(NRF_TWIM,TWIM_USED)
+#define TWIM_IRQN             TWIM_IRQN_a(TWIM_USED)
+#define TWIM_IRQ_Handler      TWIM_IRQ_Handler_a(TWIM_USED)
+
+#define TWIM_IRQN_a(n)        TWIM_IRQN_b(n)
+#define TWIM_IRQN_b(n)        SPIM##n##_SPIS##n##_TWIM##n##_TWIS##n##_SPI##n##_TWI##n##_IRQn
+
+#define TWIM_IRQ_Handler_a(n) TWIM_IRQ_Handler_b(n)
+#define TWIM_IRQ_Handler_b(n) SPIM##n##_SPIS##n##_TWIM##n##_TWIS##n##_SPI##n##_TWI##n##_IRQHandler
+/** @} */
+
+#define TWIM_EVENT_CLEAR(x)       do{ \
+         (x) = 0; \
+         (void) (x); \
+    }while(0)
+
+static inline void send_event(twim_transfer_t txfr)
+{
+    //This works because the masks are 1 shifts of the transfer types
+    if(twim_status.evt_mask | (1 << txfr))
+    {
+        twim_status.handler(TWIM_ERR_NONE, twim_status.current_transfer);
+    }
+}
+
+static void clear_all_events(void)
+{
+    TWIM_ID->EVENTS_ERROR = 0;
+    TWIM_ID->EVENTS_LASTRX = 0;
+    TWIM_ID->EVENTS_LASTTX = 0;
+    TWIM_ID->EVENTS_RXSTARTED = 0;
+    TWIM_ID->EVENTS_STOPPED = 0;
+    TWIM_ID->EVENTS_SUSPENDED = 0;
+    TWIM_ID->EVENTS_TXSTARTED = 0;
+}
+
+static void handle_error(void)
+{
+    TWIM_EVENT_CLEAR(TWIM_ID->EVENTS_ERROR);
+
+    TWIM_ID->INTENCLR = TWIM_INTENCLR_STOPPED_Msk;
+    (void)TWIM_ID->INTENCLR;
+
+    twim_err_t err;
+    if((TWIM_ID->ERRORSRC & TWIM_ERRORSRC_ANACK_Msk))
+    {
+        TWIM_ID->ERRORSRC = TWIM_ERRORSRC_ANACK_Msk;
+        err = TWIM_ERR_ADRS_NACK;
+    }
+
+    if((TWIM_ID->ERRORSRC & TWIM_ERRORSRC_DNACK_Msk))
+    {
+        TWIM_ID->ERRORSRC = TWIM_ERRORSRC_DNACK_Msk;
+        err = TWIM_ERR_DATA_NACK;
+    }
+    twim_status.handler(err, twim_status.current_transfer);
+    /**TODO The stop will not generate an interrupt only when
+    its at the end of this function. WHY??? */
+    TWIM_ID->TASKS_STOP = 1;
+}
+
+void hal_twim_init(hal_twim_init_config_t * config)
+{
+    //Configure as strong drive low and disconnect high with pull-ups
+    NRF_GPIO->PIN_CNF[config->scl] =
+            ( (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos)
+            | (GPIO_PIN_CNF_DRIVE_H0D1     << GPIO_PIN_CNF_DRIVE_Pos)
+            | (GPIO_PIN_CNF_PULL_Pullup    << GPIO_PIN_CNF_PULL_Pos)
+            | (GPIO_PIN_CNF_INPUT_Connect  << GPIO_PIN_CNF_INPUT_Pos)
+            | (GPIO_PIN_CNF_DIR_Input      << GPIO_PIN_CNF_DIR_Pos));
+    NRF_GPIO->PIN_CNF[config->sda] =
+            ( (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos)
+            | (GPIO_PIN_CNF_DRIVE_H0D1     << GPIO_PIN_CNF_DRIVE_Pos)
+            | (GPIO_PIN_CNF_PULL_Pullup    << GPIO_PIN_CNF_PULL_Pos)
+            | (GPIO_PIN_CNF_INPUT_Connect  << GPIO_PIN_CNF_INPUT_Pos)
+            | (GPIO_PIN_CNF_DIR_Input      << GPIO_PIN_CNF_DIR_Pos));
+
+    twim_status.scl = TWIM_ID->PSEL.SCL = config->scl;
+    twim_status.sda = TWIM_ID->PSEL.SDA = config->sda;
+
+    TWIM_ID->FREQUENCY = config->frequency;
+    TWIM_ID->ADDRESS = config->address;
+    //Use EasyDMA
+    TWIM_ID->TXD.LIST = TWIM_TXD_LIST_LIST_Msk;
+
+    clear_all_events();
+    NVIC_ClearPendingIRQ(TWIM_IRQN);
+    NVIC_SetPriority(TWIM_IRQN, config->irq_priority);
+    NVIC_EnableIRQ(TWIM_IRQN);
+
+    TWIM_ID->ENABLE = TWIM_ENABLE_ENABLE_Enabled << TWIM_ENABLE_ENABLE_Pos;
+    twim_status.evt_mask = config->evt_mask;
+    twim_status.handler = config->evt_handler;
+    twim_status.transfer_finished = true;
+    twim_status.on = true;
+}
+
+void hal_twim_uninit(void)
+{
+    if(twim_status.on == false){
+        return;
+    }
+    twim_status.on = false;
+    TWIM_ID->ENABLE = TWIM_ENABLE_ENABLE_Disabled << TWIM_ENABLE_ENABLE_Pos;
+
+    NVIC_ClearPendingIRQ(TWIM_IRQN);
+    NVIC_DisableIRQ(TWIM_IRQN);
+
+    //Configure back to standard drive high and low
+    NRF_GPIO->PIN_CNF[twim_status.scl] =
+            ( (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos)
+            | (GPIO_PIN_CNF_DRIVE_S0S1     << GPIO_PIN_CNF_DRIVE_Pos)
+            | (GPIO_PIN_CNF_PULL_Disabled    << GPIO_PIN_CNF_PULL_Pos)
+            | (GPIO_PIN_CNF_INPUT_Disconnect  << GPIO_PIN_CNF_INPUT_Pos)
+            | (GPIO_PIN_CNF_DIR_Input      << GPIO_PIN_CNF_DIR_Pos));
+    NRF_GPIO->PIN_CNF[twim_status.sda] =
+            ( (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos)
+            | (GPIO_PIN_CNF_DRIVE_S0S1     << GPIO_PIN_CNF_DRIVE_Pos)
+            | (GPIO_PIN_CNF_PULL_Disabled    << GPIO_PIN_CNF_PULL_Pos)
+            | (GPIO_PIN_CNF_INPUT_Disconnect  << GPIO_PIN_CNF_INPUT_Pos)
+            | (GPIO_PIN_CNF_DIR_Input      << GPIO_PIN_CNF_DIR_Pos));
+
+    TWIM_ID->ADDRESS = 0;
+}
+
+static twim_ret_status initial_txfr_check(void){
+    if(twim_status.on == false)
+    {
+        return TWIM_UNINIT;
+    }
+
+    //Starting from a previous transfer where TWIM_TRANSFER_DONE_MSK wasn't set
+    if(TWIM_ID->EVENTS_STOPPED)
+    {
+        twim_status.transfer_finished = true;
+        return TWIM_STARTED;
+    }
+
+    if(twim_status.transfer_finished == false)
+    {
+        return TWIM_BUSY;
+    }
+    return TWIM_STARTED;
+}
+
+twim_ret_status hal_twim_tx(uint8_t * tx_ptr, uint32_t tx_len)
+{
+    twim_ret_status check_val = initial_txfr_check();
+    if(check_val != TWIM_STARTED)
+    {
+        return check_val;
+    }
+
+    clear_all_events();
+    TWIM_ID->TXD.PTR = (uint32_t) tx_ptr;
+    TWIM_ID->TXD.MAXCNT = tx_len;
+    TWIM_ID->SHORTS = TWIM_SHORTS_LASTTX_STOP_Msk;
+    TWIM_ID->TASKS_STARTTX = 1;
+
+    twim_status.current_transfer = TWIM_TX;
+    twim_status.transfer_finished = false;
+
+    //Error is always set, as its required to recover from an error
+    TWIM_ID->INTEN = TWIM_INTENSET_ERROR_Msk;
+    (void)TWIM_ID->INTEN;
+    TWIM_ID->INTENSET = (twim_status.evt_mask & TWIM_TX_DONE_MSK)?
+            (TWIM_INTENSET_STOPPED_Msk) : 0 ;
+
+    return TWIM_STARTED;
+}
+
+twim_ret_status hal_twim_rx(uint8_t * rx_ptr, uint32_t rx_len)
+{
+    twim_ret_status check_val = initial_txfr_check();
+    if(check_val != TWIM_STARTED)
+    {
+        return check_val;
+    }
+
+    clear_all_events();
+    TWIM_ID->RXD.PTR = (uint32_t) rx_ptr;
+    TWIM_ID->RXD.MAXCNT = rx_len;
+    TWIM_ID->SHORTS = TWIM_SHORTS_LASTRX_STOP_Msk;
+    TWIM_ID->TASKS_STARTRX = 1;
+
+    twim_status.current_transfer = TWIM_RX;
+    twim_status.transfer_finished = false;
+
+    //Error is always set, as its required to recover from an error
+    TWIM_ID->INTEN = TWIM_INTENSET_ERROR_Msk;
+    (void)TWIM_ID->INTEN;
+    TWIM_ID->INTENSET = (twim_status.evt_mask & TWIM_RX_DONE_MSK)?
+            (TWIM_INTENSET_STOPPED_Msk) : 0 ;
+
+    return TWIM_STARTED;
+}
+
+twim_ret_status hal_twim_tx_rx(uint8_t * tx_ptr, uint32_t tx_len,
+        uint8_t * rx_ptr, uint32_t rx_len)
+{
+    twim_ret_status check_val = initial_txfr_check();
+    if(check_val != TWIM_STARTED)
+    {
+        return check_val;
+    }
+
+    clear_all_events();
+    TWIM_ID->TXD.PTR = (uint32_t) tx_ptr;
+    TWIM_ID->TXD.MAXCNT = tx_len;
+    TWIM_ID->RXD.PTR = (uint32_t) rx_ptr;
+    TWIM_ID->RXD.MAXCNT = rx_len;
+    TWIM_ID->SHORTS = TWIM_SHORTS_LASTTX_STARTRX_Msk |
+            TWIM_SHORTS_LASTRX_STOP_Msk;
+    TWIM_ID->TASKS_STARTTX = 1;
+
+    twim_status.current_transfer = TWIM_TX_RX;
+    twim_status.transfer_finished = false;
+
+    //Error is always set, as its required to recover from an error
+    TWIM_ID->INTEN = TWIM_INTENSET_ERROR_Msk;
+    (void)TWIM_ID->INTEN;
+    TWIM_ID->INTENSET = (twim_status.evt_mask & TWIM_TX_RX_DONE_MSK)?
+            (TWIM_INTENSET_STOPPED_Msk) : 0 ;
+
+    return TWIM_STARTED;
+}
+
+uint32_t hal_twim_get_current_adrs(void)
+{
+    return TWIM_ID->ADDRESS;
+}
+
+void TWIM_IRQ_Handler(void)
+{
+    if(TWIM_ID->EVENTS_ERROR == 1){
+        handle_error();
+    }
+
+    if(TWIM_ID->EVENTS_STOPPED == 1){
+        TWIM_EVENT_CLEAR(TWIM_ID->EVENTS_STOPPED);
+        twim_status.transfer_finished = true;
+
+        send_event(twim_status.current_transfer);
+    }
+}
---
+++ codebase/hal/hal_twim.h
@@ -0,0 +1,158 @@
+/*
+ *  hal_twim.h
+ *
+ *  Created on: 19-May-2017
+ *
+ *  Copyright (c) 2017, Appiko
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification,
+ *  are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its contributors
+ *  may be used to endorse or promote products derived from this software without
+ *  specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ *  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @addtogroup group_hal
+ * @{
+ *
+ * @defgroup group_twim_driver TWI Master HAL
+ * @brief Hardware abstraction layer of the Two Wire Interface in Master mode. This driver
+ *  is completely event driven and non-blocking, suitable for low power applications.
+ *
+ * @warning This module needs the LFCLK to be on and running to be able to work
+ * @{
+ */
+
+#ifndef CODEBASE_HAL_HAL_TWIM_H_
+#define CODEBASE_HAL_HAL_TWIM_H_
+
+#include "nrf.h"
+
+/** Specify which TWIM peripheral is used for this HAL module */
+#define TWIM_USED           0
+
+/** @brief Defines for TWI master clock frequency.
+ */
+typedef enum
+{
+    HAL_TWI_FREQ_100K = TWI_FREQUENCY_FREQUENCY_K100, ///< 100 kbps
+    HAL_TWI_FREQ_250K = TWI_FREQUENCY_FREQUENCY_K250, ///< 250 kbps
+    HAL_TWI_FREQ_400K = TWI_FREQUENCY_FREQUENCY_K400  ///< 400 kbps
+} hal_twim_freq_t;
+
+/** @brief Defines for the types of transfers possible.
+ */
+typedef enum {
+    TWIM_TX,    ///< Only a Tx transfer
+    TWIM_RX,    ///< Only a Rx transfer
+    TWIM_TX_RX  ///< A Tx transfer followed by Rx with a repeated start
+} twim_transfer_t;
+
+/** @brief Defines for the types of errors possible during TWI transactions.
+ */
+typedef enum {
+    TWIM_ERR_NONE,       ///< No error for this transfer
+    TWIM_ERR_ADRS_NACK,  ///< The slave device generated an error on the address bytes
+    TWIM_ERR_DATA_NACK   ///< The slave device generated an error on the data bytes
+} twim_err_t;
+
+/** @brief Defines for the return values for the transfer calls.
+ */
+typedef enum {
+    TWIM_STARTED,   ///< Transfer successfully started
+    TWIM_BUSY,      ///< A transfer is already happening
+    TWIM_UNINIT     ///< The TWIM peripheral is not initialized
+} twim_ret_status;
+
+#define TWIM_TX_DONE_MSK            (1<<TWIM_TX)
+#define TWIM_RX_DONE_MSK            (1<<TWIM_RX)
+#define TWIM_TX_RX_DONE_MSK         (1<<TWIM_TX_RX)
+
+/** @brief Structure for the TWI master driver initialization
+ */
+typedef struct
+{
+    uint32_t            scl;                 ///< SCL pin number
+    uint32_t            sda;                 ///< SDA pin number
+    hal_twim_freq_t     frequency;           ///< TWI frequency
+    uint32_t            irq_priority;        ///< Interrupt priority
+    uint32_t            address;             ///< I2C device address
+    /// This event handler is called whenever an unmasked transaction has successfully
+    /// completed or whenever an error occurs.
+    /// @note Errors cannot be masked, while the types of transfer completes can be masked
+    void (*evt_handler)(twim_err_t evt, twim_transfer_t transfer);
+    /// Event Mask for specifying which successful transfer type calls the handler
+    uint32_t            evt_mask;
+
+} hal_twim_init_config_t;
+
+/**
+ * @brief Function for initializing and enabling one of the TWIM peripheral
+ * @param config Pointer to the initialization configuration parameters
+ */
+void hal_twim_init(hal_twim_init_config_t * config);
+
+/**
+ * @brief Function for uninitializing and disabling one of the TWIM peripheral
+ */
+void hal_twim_uninit(void);
+
+/**
+ * @brief Start a Tx only TWI transfer
+ * @param tx_ptr Pointer to the data to be transferred
+ * @param tx_len Length of the data to be transferred
+ * @return Status of the transfer as per @ref twim_ret_status
+ */
+twim_ret_status hal_twim_tx(uint8_t * tx_ptr, uint32_t tx_len);
+
+/**
+ * @brief Start a Rx only TWI transfer
+ * @param rx_ptr Pointer to the data to be received
+ * @param rx_len Length of the data to be received
+ * @return Status of the transfer as per @ref twim_ret_status
+ */
+twim_ret_status hal_twim_rx(uint8_t * rx_ptr, uint32_t rx_len);
+
+/**
+ * @brief Start a Tx TWI transfer followed by a Rx by repeated start
+ * @param tx_ptr Pointer to the data to be transferred
+ * @param tx_len Length of the data to be transferred
+ * @param rx_ptr Pointer to the data to be received
+ * @param rx_len Length of the data to be received
+ * @return Status of the transfer as per @ref twim_ret_status
+ */
+twim_ret_status hal_twim_tx_rx(uint8_t * tx_ptr, uint32_t tx_len,
+        uint8_t * rx_ptr, uint32_t rx_len);
+
+/**
+ * @brief Get the current specified address of the I2C slave
+ * @return The I2C address currently initialized in the driver
+ */
+uint32_t hal_twim_get_current_adrs(void);
+
+#endif /* CODEBASE_HAL_HAL_TWIM_H_ */
+/**
+ * @}
+ * @}
+ */
