678d37c59cd9ad666f23e9180784af4f6ce108dc
Tejas Vasekar <tsvasekar@gmail.com>
2018-10-04 15:40:26 +0530

    Implement module to write and read sensepi_config from flash memory.
    This module contains the functions which can be used to write the current
    sensepi_config into flash memory. Also to read from flash memory.
    But this module cannot handle the case where if program tries to read from
    empty flash memory.
    
    Warning : main application has to take care that it'll not fetch from empty
    flash memory.
    
    TODO : Try to implement these functions with single shared pointer to make
    this module more efficient.
    
    Signed-off-by: Tejas Vasekar <tsvasekar@gmail.com>

---
+++ application/sense_pir/sensepi_store_config.c
@@ -0,0 +1,112 @@
+/* 
+ * File:   sensepi_store_config.c
+ * Copyright (c) 2018 Appiko
+ * Created on 4 October, 2018, 11:40 AM
+ * Author:  Tejas Vasekar (https://github.com/tejas-tj)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE
+ */
+
+
+#include "sensepi_store_config.h"
+
+#include "hal_nop_delay.h"
+
+#include "nrf_util.h"
+#include "nrf_assert.h"
+#include "common_util.h"
+
+#include "log.h"
+
+/** Starting of memory location to store the last configuration  */
+#define LAST_CONFIG_ADDR 0x27FE8  ///After storing configuration on this location 
+///next time before writing all the configurations will be ersed.
+/** Reset value or any flash register */
+#define MEM_RESET_VALUE 0xFFFFFFFF
+/** Size of config in unit of size of pointer */
+#define CONFIG_SIZE_TO_POINTER 5 
+
+uint32_t sensepi_store_config_get_next_location (void)
+{
+    log_printf("%s\n",__func__);
+    uint32_t *p_mem_loc;
+    p_mem_loc = (uint32_t *) SENSEPI_STORE_CONFIG_LAST_APP_PAGE_ADDR;
+    while(p_mem_loc <= (uint32_t *)LAST_CONFIG_ADDR)
+    {
+        if(*p_mem_loc == MEM_RESET_VALUE)
+        {
+            return (uint32_t)p_mem_loc;
+        }
+        p_mem_loc += CONFIG_SIZE_TO_POINTER;
+        hal_nop_delay_us(700);
+    }
+    return MEM_RESET_VALUE;
+    
+}
+
+void sensepi_store_config_write (sensepi_config_t* latest_config)
+{
+    log_printf("%s\n",__func__);
+    uint32_t * p_mem_loc = (uint32_t *) sensepi_store_config_get_next_location();
+    uint32_t * p_config_cast = (uint32_t *) latest_config;
+    if(p_mem_loc == (uint32_t *)MEM_RESET_VALUE)
+    {
+        sensepi_store_config_clear_all ();
+        p_mem_loc = (uint32_t *) sensepi_store_config_get_next_location();
+    }
+    
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
+    for(uint32_t i =0 ; i < CONFIG_SIZE_TO_POINTER; i++)
+    {
+        *p_mem_loc++ = *p_config_cast++;
+    }
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
+    while(NRF_NVMC->READY != NVMC_READY_READY_Ready);
+
+}
+
+sensepi_config_t * sensepi_store_config_get_last_config ()
+{
+    log_printf("%s\n",__func__);
+    uint32_t *p_mem_loc = (uint32_t*)sensepi_store_config_get_next_location();
+    if(p_mem_loc != (uint32_t*)SENSEPI_STORE_CONFIG_LAST_APP_PAGE_ADDR)
+    {
+        p_mem_loc -= CONFIG_SIZE_TO_POINTER;
+    }
+    return (sensepi_config_t*) p_mem_loc;
+}
+
+void sensepi_store_config_clear_all (void)
+{
+    log_printf("%s\n",__func__);
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Een;
+    while(NRF_NVMC->READY != NVMC_READY_READY_Ready);
+    NRF_NVMC->ERASEPAGE = (uint32_t)SENSEPI_STORE_CONFIG_LAST_APP_PAGE_ADDR;
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
+    while(NRF_NVMC->READY != NVMC_READY_READY_Ready);
+}
---
+++ application/sense_pir/sensepi_store_config.h
@@ -0,0 +1,92 @@
+/* 
+ * File:   sensepi_store_config.h
+ * Copyright (c) 2018 Appiko
+ * Created on 4 October, 2018, 11:40 AM
+ * Author:  Tejas Vasekar (https://github.com/tejas-tj)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE
+ */
+
+/**
+ * @addtogroup sense_appln
+ * @{
+ * 
+ * @defgroup store_sensepi_config The support code to store sensepi_configs in flash
+ * @brief Functions to store and manage sensepi_configs in non volatile memory. \
+ * This will enable the firmware to reload the previously written config in case \
+ * of power reset or system reset.
+ * @{
+ */
+
+#ifndef SENSEPI_STORE_CONFIG_H
+#define SENSEPI_STORE_CONFIG_H
+
+#include "sensepi_ble.h"
+
+/** Address of first memory location of last page available for application */
+#define SENSEPI_STORE_CONFIG_LAST_APP_PAGE_ADDR 0x27000
+
+
+/**
+ * @brief Function to get the next location where firmware will store latest \
+ * configuration.
+ * @return Free memory location address in uint32_t format.
+ * @retval 0xFFFFFFFF memory is full and firmware will now clear all the \
+ * previously saved configurations. 
+ */
+uint32_t sensepi_store_config_get_next_location (void);
+
+/**
+ * @brief Function to write the sensepi_config_t at address location received \
+ * from @ref sensepi_store_config_get_next_location() .
+ * @note all the previously stored configurations will be erased if return value \
+ * of @ref sensepi_store_config_get_next_location() is 0xFFFFFFFF 
+ * @param latest_config pointer to sensepi_config_t which is to be stored in memory.
+ */
+void sensepi_store_config_write (sensepi_config_t * latest_config);
+
+/**
+ * @breif Function to get the last sensepi_config_t stored in flash.
+ * @Warning This function cannot differentiate between single stored config and \
+ * empty flash page. So make sure that there is at least one configuration \
+ * stored in memory
+ * @return pointer to last sensepi_config_t stored in flash.
+ */
+sensepi_config_t * sensepi_store_config_get_last_config (void);
+
+/**
+ * @brief Function to erase all the previously return configurations.
+ * @note This function will get called automatically once memory is full.
+ */
+void sensepi_store_config_clear_all (void);
+
+#endif /* SENSEPI_STORE_CONFIG_H */
+/**
+ * @}
+ * @}
+ */
