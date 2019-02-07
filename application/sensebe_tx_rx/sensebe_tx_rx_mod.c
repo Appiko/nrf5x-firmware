749a59519304ff897d32817d7caf01d070a53d4f
Tejas Vasekar <tsvasekar@gmail.com>
2019-02-07 19:33:03 +0530

    Add documentation.
    Documentation is added.
    
    Some functions and variables are renamed.
    
    Signed-off-by: Tejas Vasekar <tsvasekar@gmail.com>

--- application/sensebe_tx_rx/sensebe_tx_rx_mod.c
+++ application/sensebe_tx_rx/sensebe_tx_rx_mod.c
@@ -32,6 +32,7 @@
  * POSSIBILITY OF SUCH DAMAGE
  */
 
+
 #include "sensebe_ble.h"
 #include "sensebe_tx_rx_mod.h"
 
@@ -48,86 +49,230 @@
 #include "hal_nop_delay.h"
 #include "tssp_ir_tx.h"
 
+/***********MACROS***********/
+/** Time upto which LED feedback is to be given in motion detection mode */
+#define DETECT_FEEDBACK_TIMEOUT_TICKS MS_TIMER_TICKS_MS(270000)
+/** Multiplying factor for light intensity value received from BLE structures */
+#define LIGHT_THRESHOLD_MULTIPLY_FACTOR 32
+/** Max ADC numeric output value possible */
+#define MAX_ADC_OUTPUT 4096
+/** Cycle time after which light conditions has to be checked */
+#define LIGHT_SENSE_INTERVAL_TICKS MS_TIMER_TICKS_MS(300000)
+/** Number of pulses required for sync while module is in motion sync mode.  */
+#define PULSE_REQ_FOR_SYNC 4
+/** On time for TSSP receiver while module is in motion sync mode */
+#define MOTION_SYNC_ON_TIME 150
+/** Off time for TSSP receiver while module is in motion sync mode */
+#define MOTION_SYNC_OFF_TIME 850
+
+/***********ENUMS***********/
+/** List of states for Motion Detection module. */
 typedef enum
 {
+    /**Synchronization state*/
     MOTION_SYNC,
+    /**Idle State*/
     MOTION_IDLE,
+    /**Stop State*/
     MOTION_STOP,
-    MOTION_MAX_STATE
+    /**Maximum States Limit*/
+    MAX_MOTION_STATE
 }motion_detection_states_t;
 
+/** List of all submodules which are being managed by this module */
 typedef enum
 {
+    /**Timer Module*/
     MOD_TIMER,
+    /**Motion Detection Module*/
     MOD_MOTION,
+    /**IR transmission module*/
     MOD_IR_TX,
+    /**Maximum number of modules*/
     MAX_MODS,
 }modules_t;
 
+/** List of distance settings available for IR transmitter module. */
 typedef enum 
 {
+    /**Short Distance transmission*/
     IR_SHORT,
+    /**Mid Distance transmission */
     IR_MID,
+    /**Long Distance transmission*/
     IR_LONG,
-    IR_MAX_RANGES,
+    /**Maximum number of possible distances*/
+    MAX_IR_RANGES,
 }ir_ranges_t;
 
+/** List of all possible time delays between two wake-up events. */
 typedef enum
 {
-    IR_SPEED0 = 5,
-    IR_SPEED1 = 25,
-    IR_SPEED2 = 50,
-    IR_SPEED3 = 100,
-}ir_speed_t;
+    /**Wake up time 5ms*/
+    MOD_FREQ0 = 5,
+    /**Wake up time 25ms*/
+    MOD_FREQ1 = 25,
+    /**Wake up time 50ms*/
+    MOD_FREQ2 = 50,
+    /**Wake up time 100ms*/
+    MOD_FREQ3 = 100,
+    /**Maximum number of frequencies possible*/
+    MAX_MOD_FREQ,
+}module_freq_t;
 
-#define DETECT_FEEDBACK_TIMEOUT_TICKS MS_TIMER_TICKS_MS(270000)
-
-#define LIGHT_THRESHOLD_MULTIPLY_FACTOR 32
-
-#define MAX_ADC_OUTPUT 4096
-
-#define LIGHT_SENSE_INTERVAL_TICKS MS_TIMER_TICKS_MS(300000)
-
-#define PULSE_REQ_FOR_SYNC 4
-
-#define MOTION_SYNC_ON_TIME 150
-
-#define MOTION_SYNC_OFF_TIME 850
-
-static motion_detection_states_t motion_state = MOTION_IDLE;
-
+/***********VARIABLE***********/
+/**Global variable used to check if LED feedback is required or not*/
 static uint32_t feedback_timepassed = 0;
-
+/**Global sensebe_config_t which is to be used in this module*/
 static sensebe_config_t sensebe_config;
-
-static tssp_detect_config_t tssp_detect_config;
-
+/**Array of the wake up times*/
+static uint32_t arr_module_tick_duration[] = {MOD_FREQ0, MOD_FREQ1, MOD_FREQ2, MOD_FREQ3};
+/**Array of light status flags*/
 static bool arr_is_light_ok [MAX_MODS];
-
+/**Array of flags to keep track if light check is required or not*/
 static bool arr_is_light_sense_req [MAX_MODS];
-
+/**Array of module status flag*/
 static bool arr_is_mod_on[MAX_MODS];
-
+/**Global TSSP configuration which is to be modified and reused.*/
+static tssp_detect_config_t tssp_detect_config;
+/**Global variable used to keep track of motion detection module's state*/
+static motion_detection_states_t motion_state = MOTION_IDLE;
+/**Global variable used to store value after which timer trigger should be generated*/
+static uint32_t timer_module_value = 0;
+/**Array of LED patterns related to IR transmission range */
+static led_sequences arr_range_indicator[] =
+            {LED_SEQ_RED_PULSE, LED_SEQ_ORANGE_PULSE, LED_SEQ_GREEN_PULSE,};
+/**Global variable to keep track of IR transmission range*/
+static ir_ranges_t tx_range = IR_SHORT;
+/**Global variables to store pin numbers of IR transmission LED control pins*/
+static uint32_t ir_pwr1 = 0, ir_pwr2 =0;
+/**Global variable to store Analog pin number of light sense pin*/
 static uint32_t light_check_sense_pin = 0;
-
+/**Global variable to store pin number of Light sensor control pin*/
 static uint32_t light_check_en_pin = 0;
 
-static uint32_t module_tick_duration[] = {IR_SPEED0, IR_SPEED1, IR_SPEED2, IR_SPEED3};
+/***********FUNCTIONS***********/
+/** Motion Detection Module Related Functions. */
+/**Function to start motion detection module*/
+void motion_module_start (void);
+/**Function to handle add_ticks event for motion detection module*/
+void motion_module_add_ticks (void);
+/**Function to handle add_mod_ticks event for motion detection module*/
+void motion_module_add_mod_ticks (void);
+/**Function to stop motion detection module*/
+void motion_module_stop (void);
 
-static uint32_t timer_module_value = 0;
+/**Function used to put motion detection module in Sync state*/
+void state_change_sync (void);
+/**Function used to put motion detection module in Idle state*/
+void state_change_idle (void);
+/**Function used to put motion detection module in Stop state*/
+void state_change_stop (void);
+/**Array of function pointers used to change state*/
+void (* arr_state_change[]) () = {
+    state_change_sync,
+    state_change_idle,
+    state_change_stop
+};
 
-static led_sequences range_indicator[] =
-            {LED_SEQ_RED_PULSE, LED_SEQ_ORANGE_PULSE, LED_SEQ_GREEN_PULSE,};
+/**Handler which will get provoked when window is detected*/
+void window_detect_handler ();
+/**Handler which will get provoked when pulse is detected*/
+void pulse_detect_handler (uint32_t ticks_count);
 
-static ir_ranges_t tx_range = IR_SHORT;
 
-static uint32_t ir_pwr1 = 0, ir_pwr2 =0;
+/** Timer Module Related Functions. */
+/**Function to start timer module*/
+void timer_module_start (void);
+/**Function to handle add_ticks event for timer module*/
+void timer_module_add_ticks (void);
+/**Function to handle add_mod_ticks event for timer module*/
+void timer_module_add_mod_ticks (void);
+/**Function to stop timer module*/
+void timer_module_stop (void);
 
+/**Function which is to be called when timer trigger should happen*/
+void timer_trigger_handler ();
+
+/** IR Transmitter Module Related Functions */
+/**Function to start IR transmitter module*/
+void ir_tx_module_start (void);
+/**Function to handle add_ticks event for IR transmitter module*/
+void ir_tx_module_add_ticks (void);
+/**Function to handle add_mod_ticks event for IR transmitter module*/
+void ir_tx_module_add_mod_ticks (void);
+/**Function to stop IR transmitter module*/
+void ir_tx_module_stop (void);
+
+/**Function to change IR transmission range to short range*/
+void ir_range_short (void);
+/**Function to change IR transmission range to mid range*/
+void ir_range_mid (void);
+/**Function to change IR transmission range to long range*/
+void ir_range_long (void);
+/**Array of function pointers used to change range of IR transmission*/
+void (* arr_ir_range_select[]) () ={
+    ir_range_short,
+    ir_range_mid,
+    ir_range_long,
+};
+
+/** Light Sensing Related Functions. */
 /***/
-void light_check (uint32_t interval);
+/**
+ * @brief Function to check current Light intensity
+ * @param oper_time Light conditions given by BLE config
+ * @param light_intensity current Light intensity
+ * @param module Module for which this light testing is being done.
+ */
+void light_check (oper_time_t oper_time, uint32_t light_intensity, uint32_t module);
+/**
+ * @brief Function to handle add_ticks_event for Light sensing module
+ * @param interval Number of MS_TIMER_TICKS happened since last event
+ */
+void light_sense_add_ticks (uint32_t interval);
 
+/** Support Functions */
+/**
+ * @brief Callbcak function which will be called when cam_trigger module finishes it's operation.
+ * @param trigger Module which triggered this cam_trigger operation
+ */
+void camera_unit_handler(uint32_t trigger);
+/**
+ * @brief Function to compare two values with some margin
+ * @param data Data which is to be compared
+ * @param ref Reference with which data is to be compared
+ * @param margin Value by which data can vary from reference without failing the test
+ * @return comparison result
+ * @retval true Data is within the margins from reference
+ * @retval false Data is not within the margins from reference
+ */
+bool compare_margin(uint32_t data, uint32_t ref, uint32_t margin);
+/**
+ * @brief Function to see if four pulses approximately have same time difference   
+ * @param ticks Ticks since last pulse.
+ * @return Result of pulse duration comparison.
+ * @retval true Four pulses have approximately equal delay.
+ * @retval false Fout pulses do not have approximately equal delay. 
+ */
+bool three_window_sync (uint32_t ticks);
+/**
+ * @brief Function to handle add_tick event for LED feedback functionality.
+ * @param interval MS_TIMER_TICKS since last add_ticks event
+ */
+void add_ticks_feedback (uint32_t interval);
+
+/** Module Clock Handler */
+/**
+ * @brief Function to handle module ticks.
+ * @description Modules Clock is the clock at which this module is working.\
+ * @ref module_freq_t for different operational frequencies for this module.\
+ * A ms_timer will be running in repeated mode generating pulses with given freq.
+ */
 void module_tick_handler ();
 
+
+/*******************Definitions*******************/
 void add_ticks_feedback (uint32_t interval)
 {
     feedback_timepassed += interval;
@@ -158,12 +303,6 @@ void state_change_stop ()
     tssp_detect_pulse_stop ();
 }
 
-void (* arr_state_change[]) () = {
-    state_change_sync,
-    state_change_idle,
-    state_change_stop
-};
-
 void ir_range_short ()
 {
     log_printf("%s\n",__func__);
@@ -185,12 +324,6 @@ void ir_range_long ()
     hal_gpio_pin_write (ir_pwr2, 1);
 }
 
-void (* ir_range_select[]) () ={
-    ir_range_short,
-    ir_range_mid,
-    ir_range_long,
-};
-
 void camera_unit_handler(uint32_t trigger)
 {
     log_printf("%s\n", __func__);
@@ -205,8 +338,7 @@ void camera_unit_handler(uint32_t trigger)
 }
 
 void timer_trigger_handler ()
-{
-    
+{   
     if(cam_trigger_is_on () == false)
     {
         cam_trigger (MOD_TIMER);
@@ -215,17 +347,15 @@ void timer_trigger_handler ()
 
 void window_detect_handler ()
 {
-    {
-        led_ui_stop_seq (LED_UI_LOOP_SEQ, LED_SEQ_DETECT_PULSE);
-        led_ui_single_start (LED_SEQ_DETECT_WINDOW, LED_UI_MID_PRIORITY, true);
-        motion_state = MOTION_SYNC;
-        arr_state_change[motion_state] ();
-        tssp_detect_pulse_detect ();
-        cam_trigger (MOD_MOTION);
-    }
+    led_ui_stop_seq (LED_UI_LOOP_SEQ, LED_SEQ_DETECT_PULSE);
+    led_ui_single_start (LED_SEQ_DETECT_WINDOW, LED_UI_MID_PRIORITY, true);
+    motion_state = MOTION_SYNC;
+    arr_state_change[motion_state] ();
+    tssp_detect_pulse_detect ();
+    cam_trigger (MOD_MOTION);
 }
 
-bool compare_percent(uint32_t data, uint32_t ref, uint32_t margin)
+bool compare_margin(uint32_t data, uint32_t ref, uint32_t margin)
 {
     if((ref-margin)<=data && data<=(ref+margin))
     {
@@ -237,7 +367,7 @@ bool compare_percent(uint32_t data, uint32_t ref, uint32_t margin)
     }
 }
 
-bool two_window_sync (uint32_t ticks)
+bool three_window_sync (uint32_t ticks)
 {
     static uint32_t previous_pulse_tick = 0, current_pulse_tick = 0;
     static uint32_t pulse_diff_window[] = {0,0,0}, pulse_diff_window_cnt = 0;
@@ -263,8 +393,8 @@ bool two_window_sync (uint32_t ticks)
         log_printf("Window[1]: %d\n", pulse_diff_window[1]);
         log_printf("Window[2]: %d\n", pulse_diff_window[2]);
         pulse_cnt = PULSE_REQ_FOR_SYNC;
-        return (compare_percent (pulse_diff_window[1], pulse_diff_window[0], 2)
-            && compare_percent (pulse_diff_window[2], pulse_diff_window[1], 2));
+        return (compare_margin (pulse_diff_window[1], pulse_diff_window[0], 2)
+            && compare_margin (pulse_diff_window[2], pulse_diff_window[1], 2));
     }
     
     return false;
@@ -274,7 +404,7 @@ void pulse_detect_handler (uint32_t ticks_count)
 {
     if(motion_state == MOTION_SYNC)
     {
-        if(two_window_sync (ticks_count))
+        if(three_window_sync (ticks_count))
         {
             motion_state = MOTION_IDLE;
             arr_state_change[motion_state] ();
@@ -295,7 +425,7 @@ void pulse_detect_handler (uint32_t ticks_count)
     }
 }
 
-void light_sense (oper_time_t oper_time, uint32_t light_intensity, uint32_t trigger)
+void light_check (oper_time_t oper_time, uint32_t light_intensity, uint32_t module)
 {
     uint8_t light_sense_config = oper_time.day_or_night;
     uint32_t light_threshold =
@@ -308,15 +438,15 @@ void light_sense (oper_time_t oper_time, uint32_t light_intensity, uint32_t trig
        ((light_sense_config == 0) && (light_intensity <= light_threshold)))
     //assgin to respective light flag
     {
-        arr_is_light_ok[trigger] = true;
+        arr_is_light_ok[module] = true;
     }
     else
     {
-        arr_is_light_ok[trigger] = false;
+        arr_is_light_ok[module] = false;
     }
 }
 
-void light_check (uint32_t interval)
+void light_sense_add_ticks (uint32_t interval)
 {
     static uint32_t timepassed = 0;
     timepassed += interval;
@@ -332,18 +462,18 @@ void light_check (uint32_t interval)
         //motion light check
         if(arr_is_light_sense_req[MOD_MOTION])
         {
-            light_sense (sensebe_config.tssp_conf.oper_time, light_intensity, MOD_MOTION);
+            light_check (sensebe_config.tssp_conf.oper_time, light_intensity, MOD_MOTION);
         }
         
         //timer light check
         if(arr_is_light_sense_req[MOD_TIMER])
         {
-            light_sense (sensebe_config.timer_conf.oper_time, light_intensity, MOD_TIMER);
+            light_check (sensebe_config.timer_conf.oper_time, light_intensity, MOD_TIMER);
         }
         
         if(arr_is_light_sense_req[MOD_IR_TX])
         {
-            light_sense (sensebe_config.ir_tx_conf.oper_time, light_intensity, MOD_IR_TX);
+            light_check (sensebe_config.ir_tx_conf.oper_time, light_intensity, MOD_IR_TX);
         }
         
         //Disable light sense module
@@ -402,7 +532,7 @@ void motion_module_add_mod_ticks ()
 {
     static bool is_rx_on = true;
     static uint32_t mod_ticks;
-    mod_ticks += module_tick_duration[sensebe_config.ir_tx_conf.ir_tx_speed];
+    mod_ticks += arr_module_tick_duration[sensebe_config.ir_tx_conf.ir_tx_speed];
     if(motion_state == MOTION_SYNC)
     {
         if(is_rx_on == true && mod_ticks >= MOTION_SYNC_ON_TIME)
@@ -479,10 +609,11 @@ void timer_module_add_ticks ()
 void timer_module_add_mod_ticks ()
 {
     static uint32_t mod_ticks;
-    mod_ticks += module_tick_duration[sensebe_config.ir_tx_conf.ir_tx_speed];
-    if(mod_ticks > timer_module_value)
+    mod_ticks += arr_module_tick_duration[sensebe_config.ir_tx_conf.ir_tx_speed];
+    if(mod_ticks >= timer_module_value)
     {
         timer_trigger_handler ();
+        mod_ticks = 0;
     }
 }
 
@@ -508,14 +639,14 @@ void ir_tx_module_start ()
         arr_is_light_sense_req[MOD_IR_TX] = true;
     }      
     
-    if(sensebe_config.ir_tx_conf.ir_tx_pwr == IR_MAX_RANGES)
+    if(sensebe_config.ir_tx_conf.ir_tx_pwr == MAX_IR_RANGES)
     {
         sensebe_config.ir_tx_conf.ir_tx_pwr = IR_LONG;
     }
 
     tx_range = sensebe_config.ir_tx_conf.ir_tx_pwr;
-    ir_range_select[tx_range]();    
-    led_ui_single_start (range_indicator[tx_range], LED_UI_LOW_PRIORITY, true);
+    arr_ir_range_select[tx_range]();    
+    led_ui_single_start (arr_range_indicator[tx_range], LED_UI_LOW_PRIORITY, true);
 
     arr_is_mod_on[MOD_IR_TX] = true;
 }
@@ -644,10 +775,10 @@ void sensebe_tx_rx_start (void)
     }
     
     ms_timer_start (SENSEBE_OPERATION_MS_TIMER, MS_REPEATED_CALL,
-        MS_TIMER_TICKS_MS(module_tick_duration[sensebe_config.ir_tx_conf.ir_tx_speed])
+        MS_TIMER_TICKS_MS(arr_module_tick_duration[sensebe_config.ir_tx_conf.ir_tx_speed])
         , module_tick_handler);
     
-    light_check (LIGHT_SENSE_INTERVAL_TICKS);
+    light_sense_add_ticks (LIGHT_SENSE_INTERVAL_TICKS);
 }
 
 void sensebe_tx_rx_stop (void)
@@ -668,7 +799,7 @@ void sensebe_tx_rx_add_ticks (uint32_t interval)
        arr_is_light_sense_req [MOD_TIMER] == true ||
        arr_is_light_sense_req [MOD_IR_TX])
     {
-        light_check (interval);
+        light_sense_add_ticks (interval);
     }
     
     if(sensebe_config.trig_conf != TIMER_ONLY)
@@ -697,9 +828,9 @@ sensebe_config_t * sensebe_tx_rx_last_config ()
 
 void sensebe_tx_rx_swicht_range ()
 {
-    tx_range = (tx_range + 1)%IR_MAX_RANGES;
-    led_ui_single_start (range_indicator[tx_range], LED_UI_LOW_PRIORITY, true);
+    tx_range = (tx_range + 1)%MAX_IR_RANGES;
+    led_ui_single_start (arr_range_indicator[tx_range], LED_UI_LOW_PRIORITY, true);
 
-    ir_range_select[tx_range]();
+    arr_ir_range_select[tx_range]();
     sensebe_config.ir_tx_conf.ir_tx_pwr = tx_range;
 }
