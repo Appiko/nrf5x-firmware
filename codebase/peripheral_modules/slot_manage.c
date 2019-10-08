f9c4dfc97d7c4bdbab3257aae18eed8ec3189334
Tejas Vasekar <tsvasekar@gmail.com>
2019-10-08 12:40:20 +0530

    Rename files and APIs to slot_manage
    Signed-off-by: Tejas Vasekar <tsvasekar@gmail.com>

--- codebase/peripheral_modules/slot_manage.c
+++ codebase/peripheral_modules/slot_manage.c
@@ -1,5 +1,5 @@
 /*
- *  oper_manage.c : Module to keep track of operation conditions for a device
+ *  slot_manage.c : Module to keep track of operation conditions for a device
  *  Copyright (C) 2019  Appiko
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -16,33 +16,33 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "oper_manage.h"
+#include "slot_manage.h"
 #include "simple_adc.h"
 #include "common_util.h"
 
 static uint32_t light_sense_pin;
 
-static uint32_t arr_start_cond [OPER_MANAGE_MAX_SLOTS];
-static uint32_t arr_end_cond [OPER_MANAGE_MAX_SLOTS];
+static uint32_t arr_start_cond [SLOT_MANAGE_MAX_SLOTS];
+static uint32_t arr_end_cond [SLOT_MANAGE_MAX_SLOTS];
 
-static oper_list_t arr_oper_cond [OPER_MANAGE_MAX_SLOTS];
+static slot_list_t arr_slot_cond [SLOT_MANAGE_MAX_SLOTS];
 
-volatile uint8_t active_slots = OPER_MANAGE_INVALID_SLOTS;
+volatile uint8_t active_slots = SLOT_MANAGE_INVALID_SLOTS;
 
-void oper_manage_set_light_sense_pin (uint32_t light_sense_adc)
+void slot_manage_set_light_sense_pin (uint32_t light_sense_adc)
 {
     light_sense_pin = light_sense_adc;
     
 }
 
 
-void oper_manage_set_slot (oper_manage_slot_t * new_slot)
+void slot_manage_set_slot (slot_manage_slot_t * new_slot)
 {
-    if(new_slot->slot_no < OPER_MANAGE_MAX_SLOTS)
+    if(new_slot->slot_no < SLOT_MANAGE_MAX_SLOTS)
     {
         arr_start_cond[new_slot->slot_no] = new_slot->start_cond;
         arr_end_cond[new_slot->slot_no] = new_slot->end_cond;
-        arr_oper_cond[new_slot->slot_no] = new_slot->oper_sel;
+        arr_slot_cond[new_slot->slot_no] = new_slot->slot_sel;
         uint32_t current_time = time_tracker_get_current_time_s();
         if(current_time >=  new_slot->start_cond 
            && current_time <= new_slot->end_cond)
@@ -52,17 +52,17 @@ void oper_manage_set_slot (oper_manage_slot_t * new_slot)
     }
 }
 
-uint8_t oper_manage_check_update ()
+uint8_t slot_manage_check_update ()
 {
     uint32_t current_time = time_tracker_get_current_time_s();
     uint32_t light_val = simple_adc_get_value (SIMPLE_ADC_GAIN1_6, light_sense_pin);
-    for(uint32_t slot_no = 0; slot_no < OPER_MANAGE_MAX_SLOTS; slot_no++)
+    for(uint32_t slot_no = 0; slot_no < SLOT_MANAGE_MAX_SLOTS; slot_no++)
     {
         if(arr_start_cond[slot_no] == arr_end_cond[slot_no])
         {
                 active_slots = CLR_BIT_VAR(active_slots, slot_no);
         }
-        else if(arr_oper_cond[slot_no] == OPER_MANAGE_TIME_OF_DAY)
+        else if(arr_slot_cond[slot_no] == SLOT_MANAGE_TIME_OF_DAY)
         {
             if(current_time >= arr_start_cond[slot_no] 
                && current_time <= arr_end_cond[slot_no])
@@ -74,7 +74,7 @@ uint8_t oper_manage_check_update ()
                 active_slots = CLR_BIT_VAR(active_slots, slot_no);
             }
         }
-        else if(arr_oper_cond[slot_no] == OPER_MANAGE_AMBI_LIGHT)
+        else if(arr_slot_cond[slot_no] == SLOT_MANAGE_AMBI_LIGHT)
         {
             if(light_val >= arr_start_cond[slot_no] 
                && light_val <= arr_end_cond[slot_no])
@@ -90,7 +90,7 @@ uint8_t oper_manage_check_update ()
     return active_slots;
 }
 
-uint8_t oper_manage_get_active_slots ()
+uint8_t slot_manage_get_active_slots ()
 {
     return active_slots;
 }
--- codebase/peripheral_modules/slot_manage.h
+++ codebase/peripheral_modules/slot_manage.h
@@ -1,5 +1,5 @@
 /*
- *  oper_manage.h : Module to keep track of operation conditions for a device
+ *  slot_manage.h : Module to keep track of operation conditions for a device
  *  Copyright (C) 2019  Appiko
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -16,13 +16,13 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef CODEBASE_PERIPHERAL_MODULES_OPER_MANAGE_H_
-#define CODEBASE_PERIPHERAL_MODULES_OPER_MANAGE_H_
+#ifndef CODEBASE_PERIPHERAL_MODULES_SLOT_MANAGE_H_
+#define CODEBASE_PERIPHERAL_MODULES_SLOT_MANAGE_H_
 /**
  * @addtogroup group_peripheral_modules
  * @{
  *
- * @defgroup group_oper_manage Operational condition management
+ * @defgroup group_slot_manage Operational condition management
  *
  * @brief Module to keep track of operational conditions (i.e. Time of day, 
  * ambient light) of Appiko device.
@@ -33,61 +33,61 @@
 #include "time_tracker.h"
 
 /** Maximum number of slots which can be managed by the module */
-#define OPER_MANAGE_MAX_SLOTS 8
+#define SLOT_MANAGE_MAX_SLOTS 8
 
 /** If all slots are active then module is not working properly */
-#define OPER_MANAGE_INVALID_SLOTS 0xFF
+#define SLOT_MANAGE_INVALID_SLOTS 0xFF
 
 /** List of operation conditions which are being managed by the module */
 typedef enum
 {
     /** Operation Condition : Ambient light cut-off */
-    OPER_MANAGE_AMBI_LIGHT,
+    SLOT_MANAGE_AMBI_LIGHT,
     /** Operation Condition : Time of day */
-    OPER_MANAGE_TIME_OF_DAY,
-}oper_list_t;
+    SLOT_MANAGE_TIME_OF_DAY,
+}slot_list_t;
 
 /** Structure to store Condition slots information */
 typedef struct
 {
-    /** Operation condition selection @ref oper_list_t */
-    oper_list_t oper_sel;
-    /** Slot number for given condition. Should be less than @ref OPER_MANAGE_MAX_SLOTS */
+    /** Operation condition selection @ref slot_list_t */
+    slot_list_t slot_sel;
+    /** Slot number for given condition. Should be less than @ref SLOT_MANAGE_MAX_SLOTS */
     uint32_t slot_no;
     /** Starting condition for slot */
     uint32_t start_cond;
     /** Ending condition for slot */
     uint32_t end_cond;
-}oper_manage_slot_t;
+}slot_manage_slot_t;
 
 /**
  * @brief Function to initiate new operation condition slot
- * @param new_slot Structure pointer to data type @ref oper_manage_slot_t
+ * @param new_slot Structure pointer to data type @ref slot_manage_slot_t
  * storing new slots information
  */
-void oper_manage_set_slot (oper_manage_slot_t * new_slot);
+void slot_manage_set_slot (slot_manage_slot_t * new_slot);
 
 /**
  * @breif Function to get status of slots
  * @return bitwise status of all the slots. 
  * @retval if [i]th bit is 1 then [i]th condition slot is active.
  */
-uint8_t oper_manage_get_active_slots ();
+uint8_t slot_manage_get_active_slots ();
 
 /**
  * @brief Function to check if there are any changes in active slots.
  * @return bitwise status of all slots.
  * @retval if [i]th bit is 1 then [i]th condition slot is active.
  */
-uint8_t oper_manage_check_update ();
+uint8_t slot_manage_check_update ();
 
 /**
  * @brief Function to store ADC pin number
  * @param light_sense_adc ADC pin number. @ref simple_adc_input_t 
  */
-void oper_manage_set_light_sense_pin (uint32_t light_sense_adc);
+void slot_manage_set_light_sense_pin (uint32_t light_sense_adc);
 
-#endif /* CODEBASE_PERIPHERAL_MODULES_OPER_MANAGE_H_ */
+#endif /* CODEBASE_PERIPHERAL_MODULES_SLOT_MANAGE_H_ */
 
 /**
  * @}
